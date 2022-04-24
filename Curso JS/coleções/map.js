0469f8a5f3ee8e3a9cf705a4879d973130c97711
joaomachado232 <joao.machado2@estudante.ufla.br>
2022-04-24 19:45:07 -0300

    Coleções

---
+++ Curso JS/coleções/map.js
@@ -0,0 +1,9 @@
+const myMap = new Map()
+ 
+myMap.set('apple','fruit');
+   // Map(1) {"apple" => "fruit"}
+
+myMap.get(apple); //para pegar
+    //Fruit"
+    
+myMap.delete(apple) // para apagar
---
+++ Curso JS/coleções/set.js
@@ -0,0 +1,17 @@
+// Set é uma coleção com valores únicos que não se repetem nunca!
+
+const mySet = new Set();
+
+//adicionar
+mySet.add(1);
+mySet.add(5);
+
+//consulta
+mySet.has(1);
+ // true
+
+mySet.has(3);
+//false
+
+//Deleta
+mySet.delete(5);
